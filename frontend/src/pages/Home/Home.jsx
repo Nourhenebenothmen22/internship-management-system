67986d75d34782e4893f7b6ec2fd17ff2bdcac8c
nourhene <benothmennourhen8@gmail.com>
2025-12-08 10:25:45 +0100

    Update Home page with new components and layout

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Hero from '../../components/Hero/Hero'
+import Internships from '../../components/Internships/Internships'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Hero/>
+      <Internships/>
+      
+
+    </div>
   )
 }
 
