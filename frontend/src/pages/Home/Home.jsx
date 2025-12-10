3c3c14d3407fe64c23855dab5b5f52f308693ddf
nourhene <benothmennourhen8@gmail.com>
2025-12-10 20:27:54 +0100

    Refactor Home page layout

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
 import Internships from '../../components/Internships/Internships'
+import Companies from '../Companies/Companies'
+
+
+
 
 function Home() {
   return (
     <div>
-      <Hero/>
-      <Internships/>
-      
-
+      <Hero />
+      <Internships />
     </div>
   )
 }
