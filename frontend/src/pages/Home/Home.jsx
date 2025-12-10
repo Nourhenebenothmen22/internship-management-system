e0813048f04d16d07272403d5363c908fda24bce
nourhene <benothmennourhen8@gmail.com>
2025-12-10 22:04:16 +0100

    Integrate Testimonials section into Home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
 import Internships from '../../components/Internships/Internships'
-import Companies from '../Companies/Companies'
-
-
+import Testimonials from '../../components/Testimonials/Testimonials';
 
 
 function Home() {
@@ -11,6 +9,7 @@ function Home() {
     <div>
       <Hero />
       <Internships />
+      <Testimonials />
     </div>
   )
 }
